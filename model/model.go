e4d1922ac35af922347c9dd4fae3e58c88128dfc
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-07-09 14:58:47 -0500

    set ID field to readonly

--- model/model.go
+++ model/model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type User struct {
-	ID        uint   `json:"id" gorm:"primaryKey"`
+	ID        uint   `json:"id" gorm:"primaryKey;->"`
 	Name      string `json:"name" gorm:"not null;type:varchar(255)"`
 	Email     string `json:"email" gorm:"not null;type:varchar(255)"`
 	Password  string `json:"password" gorm:"not null;type:varchar(255)"`
